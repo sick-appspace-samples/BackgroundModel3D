b551e77b2f8cc54f9692e0b12fc85217cb1577a9
SICKAppSpace-dev <>
2023-12-20 23:52:50 +0000

    Update

--- BackgroundModel3D/scripts/BackgroundModel3D.lua
+++ BackgroundModel3D/scripts/BackgroundModel3D.lua
@@ -36,14 +36,15 @@ local v3d = View.create("viewer3D1")
 
 -- Create a decoration object for visualizing the height data.
 -- The provided data is not calibrated and the Ranger 3 sensor is 832 pixels high.
-local imagedecoration = View.ImageDecoration.create()
-imagedecoration:setRange(0, 832)
+local imagedecoration = View.ImageDecoration.create():setRange(0, 832)
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
--- Helper function for showing the image results
+---Helper function for showing the image results
+---@param refimage Image
+---@param objects Image.PixelRegion[]
 local function drawResults(refimage, objects)
 
   -- Clear viewers and add images
@@ -53,55 +54,51 @@ local function drawResults(refimage, objects)
   local id3d = v3d:addHeightmap(refimage, imagedecoration)
 
   if objects then
-  
+
     -- Create a centroid marker
     local cross = {
       Shape.createLineSegment(Point.create(-10, -10), Point.create(10, 10)),
       Shape.createLineSegment(Point.create(-10, 10), Point.create(10, -10))}
-    
+
     -- Create a label decorator
-    local textdeco = View.TextDecoration.create()
-    textdeco:setColor(255, 255, 255)
-    textdeco:setSize(30)
-    
+    local textdeco = View.TextDecoration.create():setColor(255, 255, 255):setSize(30)
+
     -- Visualize the bounding boxes and centroids
-    local shapedeco = View.ShapeDecoration.create()
-    shapedeco:setLineColor(180, 10, 10)
-    shapedeco:setLineWidth(11)
-    
+    local shapedeco = View.ShapeDecoration.create():setLineColor(180, 10, 10):setLineWidth(11)
+
     -- Paint the image region
-    local regiondeco = View.PixelRegionDecoration.create()
-    regiondeco:setColor(0, 120, 220, 150)
-    
+    local regiondeco = View.PixelRegionDecoration.create():setColor(0, 120, 220, 150)
+
     -- Draw each object individually
     for i, region in ipairs(objects) do
-    
+
       -- Clean up the region
       region = region:erode(11):dilate(11)
-      
+
       -- Get the object bounds in 2D
       local bbx2D = region:getBoundingBox(refimage)
       local x, y = bbx2D:getCenterOfGravity():getXY()
-      
+
       -- Create final indicator for pick position in 2D
       local cross2D = Shape.transform(cross, Transform.createTranslation2D(x, y))
-      
+
       -- Get the height a quick and easy way
-      local z = Image.toWorldZ(refimage, refimage:getPixel(refimage:toPixelCoordinate(Point.create(x, y)):getXY()))
-      
+        ---@diagnostic disable-next-line: missing-parameter
+        local z = Image.toWorldZ(refimage, refimage:getPixel(refimage:toPixelCoordinate(Point.create(x, y)):getXY()))
+
       -- Construct 3D versions for the 3D viewer
       local bbx3D = Shape.toShape3D(bbx2D, Transform.createTranslation3D(0,0,z))
       local cross3D = Shape.toShape3D(cross, Transform.createTranslation3D(x, y, z))
-      
+
       -- The text can be the same for 2D and 3D as long as it's above the heightmap surface
       textdeco:setPosition(x + 40, y - 10, z + 2)
-      
+
       -- Draw 2D feedback
       v2d:addPixelRegion(region, regiondeco)
       v2d:addShape(cross2D, shapedeco)
       v2d:addShape(bbx2D, shapedeco)
       v2d:addText(tostring(i), textdeco)
-      
+
       -- Draw 3D feedback
       v3d:addPixelRegion(region, regiondeco, nil, id3d)
       v3d:addShape(cross3D, shapedeco)
@@ -109,13 +106,14 @@ local function drawResults(refimage, objects)
       v3d:addText(tostring(i), textdeco)
     end
   end
-  
+
   -- Send data to viewers
   v2d:present()
   v3d:present()
 end
 
--- Handle each captured image
+---Handle each captured image
+---@param image Image
 local function callback(image)
 
   -- Results handles for the foreground and region to update
@@ -126,10 +124,10 @@ local function callback(image)
   if not bg:isEmpty() then
     -- Use the model to extract the foreground
     fg = bg:compare(image, "BRIGHTER", meanThreshold, varThreshold)
-    
+
     -- Find separate blobs of certain size
     fg = fg:findConnected(10000)
-    
+
     -- Remember foreground pixels to avoid adding it to the background
     -- Only ignore large connected regions (not noise)
     roi = Image.PixelRegion.invert(fg:getUnion():dilate(5), image)
@@ -139,7 +137,7 @@ local function callback(image)
   -- The roi is used to tell the algorithm what region is to be
   -- updated. This allows us to avoid learning foreground.
   bg:add(image, roi)
-  
+
   -- Display a visualization
   drawResults(image, fg)
 
@@ -149,15 +147,16 @@ local function main()
   -- Use this simple function to keep the framerate
   -- we could also have used a Timer object.
   local tic = DateTime.getTimestamp()
+  ---@param hz int
   local function pace(hz)
     local toc = DateTime.getTimestamp()
     local sleeptime = 1000/hz - (toc-tic)
     Script.sleep(math.max(0, sleeptime))
     tic = toc
   end
-  
+
   local images = Object.load('resources/linescan.json')
-  
+
   -- Loop a few iterations
   for i = 1, 10 do
     -- Loop over all images
@@ -166,7 +165,7 @@ local function main()
       pace(1)
     end
   end
-  
+
 end
 --The following registration is part of the global scope which runs once after startup
 --Registration of the 'main' function to the 'Engine.OnStarted' event
